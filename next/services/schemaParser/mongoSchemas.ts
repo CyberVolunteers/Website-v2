b6db373f22d09c4fa8a4a1f160a9cc1eaba28d21
micnekr <misha.nekrasov@gmail.com>
2021-07-22 18:19:55 +0100

    converted everything to typescript (although there are a lot of 'any's

--- next/services/schemaParser/mongoSchemas.ts
+++ next/services/schemaParser/mongoSchemas.ts
@@ -1,4 +1,5 @@
-import { Schema } from "mongoose";
+import { PreMiddlewareFunction, Schema } from "mongoose";
+import { FieldConstraintsCollection } from "../config/shared/FieldTypes";
 import { numberValidate, stringValidate } from "./mongoValidateUtils"
 
 const fieldTypesByTypeName = {
@@ -9,52 +10,56 @@ const fieldTypesByTypeName = {
     object: Object,
 }
 
-export function deepAssign(target, src) {
+export function deepAssign<T>(target: T, src: T) {
     if (src === undefined) return target; // return target if there is nothing in src to override target with
-    const isRecursible = (val) => typeof val === "object";
-    if (!isRecursible(src) || !isRecursible(target)) return src; // if we can not recurse anymore, then choose src
+    const isRecursible = (val: any): val is object => typeof val === "object";
+    if (!isRecursible(target) || !isRecursible(src)) return src; // if we can not recurse anymore, then choose src
 
     const keys = new Set(Object.keys(target))
     Object.keys(src).forEach((k) => keys.add(k)); // a set allows us not to bother about duplicates
 
     const out = (Array.isArray(target)) ? [] : {}; // the original Object.assign seems to select the type based on the target
     keys.forEach((k) => {
-        out[k] = deepAssign(target[k], src[k])
+        (out as any)[k] = deepAssign((target as any)[k], (src as any)[k])
     })
-    return out;
+    return out as T;
 }
 
-export function constructSchema(fieldsPublic, fieldsPrivate = {}) {
+export function constructSchema(fieldsPublic: FieldConstraintsCollection, fieldsPrivate: FieldConstraintsCollection = {}) {
 
     // since Object.assign gives us a shallow copy, we can't use it
     const fields = deepAssign(fieldsPublic, fieldsPrivate);
-    const schemaSettings = {};
-    let multiParams = [];
+    const schemaSettings: any = {};
+    let multiParams: any[] = [];
 
-    Object.entries(fields).filter(([key, value]) => ["required", "optional"].includes(key)).forEach(([typeContainerKey, typeContainer]) => {
+
+    for (let typeContainerKey in fields) {
+        if (!fields.hasOwnProperty(typeContainerKey)) continue;
+        const typeContainer = (fields as any)[typeContainerKey];
         const isRequired = typeContainerKey === "required";
 
         Object.entries(fieldTypesByTypeName).forEach(([groupName, type]) => {
             // if an object, recurse
             if (!typeContainer[groupName]) return;
-            Object.entries(typeContainer[groupName]).forEach(([entryName, additionalData]) => {
+            Object.entries(typeContainer[groupName]).forEach(([entryName, additionalData]: [a: string, b: any]) => {
+
                 if (groupName === "object") return schemaSettings[entryName] = {
                     type: constructSchema(additionalData),
                     required: isRequired,
                 }
-                const currentEntry = { type, required: isRequired };
+                const currentEntry: any = { type, required: isRequired };
 
                 // if it is a sub-document, then add it
                 // callback can be left undefined to set the value directly
-                function applyCallback(settingName, callback, targetKey = settingName) {
+                function applyCallback(settingName: string, callback: any, targetKey = settingName) {
                     if (additionalData[settingName] !== undefined) currentEntry[targetKey] = callback(additionalData[settingName]);
                 }
 
-                function applyValue(settingName, targetKey = settingName) {
+                function applyValue(settingName: string, targetKey = settingName) {
                     if (additionalData[settingName] !== undefined) currentEntry[targetKey] = additionalData[settingName]; // "if" so that we don't leave "undefined" keys there
                 }
 
-                function rememberMultiParam(settingName, validator) {
+                function rememberMultiParam(settingName: string, validator: (fields: any[]) => PreMiddlewareFunction<any>) {
                     const otherParams = additionalData[settingName];
                     if (otherParams === undefined) return;
                     const outVals = [validator, entryName];
@@ -73,7 +78,7 @@ export function constructSchema(fieldsPublic, fieldsPrivate = {}) {
                 schemaSettings[entryName] = currentEntry;
             })
         })
-    })
+    }
 
     const out = new Schema(schemaSettings)
     multiParams.forEach((settings) => {
