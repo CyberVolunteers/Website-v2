c5a1c35f686b6f12d754ade9c45bff0f01cc19bb
micnekr <misha.nekrasov@gmail.com>
2021-08-10 15:14:37 +0100

    fixed a rogue import

--- next/client/components/AutoCostructedForm.tsx
+++ next/client/components/AutoCostructedForm.tsx
@@ -287,27 +287,3 @@ function extractAndValidateFormData(formStates: any, fieldStructure: Flattened):
 
     return [errors, perElementErrors, cleanedOutputData];
 }
-
-// validation
-function createIsEmailIsAvailableValidator(overallErrors: {
-    [key: string]: string;
-},
-    setOverallErrors: Dispatch<SetStateAction<{
-        [key: string]: string;
-    }>>) {
-    return async function (email: string) {
-        const res = await fetch(`/api/isEmailFree?${new URLSearchParams({ email })}`, {
-            method: "GET",
-            credentials: "same-origin", // only send cookies for same-origin requests
-            headers: {
-                "content-type": "application/json",
-                "accept": "application/json",
-            },
-        })
-
-        if (!await updateOverallErrorsForRequests(res, "isUserEmailAvailable", overallErrors, setOverallErrors)) return false;
-
-        if (await res.json() !== true) throw new Error("This email is not available") // fail-safe - if something goes wrong, it shows the warning
-        return true
-    }
-}
